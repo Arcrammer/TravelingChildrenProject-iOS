6978b6c781fcf2f98833b435fc7c8081f7a92560
Alexander Rhett Crammer <alexander2475914@gmail.com>
2017-04-16 21:01:40 -0400

    Initial Commit

--- Traveling Children Project/SecondViewController.swift
+++ Traveling Children Project/SecondViewController.swift
@@ -1,15 +1,12 @@
-//
 //  SecondViewController.swift
 //  Traveling Children Project
 //
 //  Created by Alexander Rhett Crammer on 4/16/17.
 //  Copyright © 2017 Traveling Children Project. All rights reserved.
 //
-
 import UIKit
 
 class SecondViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +16,4 @@ class SecondViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
-

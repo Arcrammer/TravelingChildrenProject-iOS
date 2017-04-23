9051f652fbfb42400698c4ea00b3cfcb7beb9abf
Alexander Rhett Crammer <alexander2475914@gmail.com>
2017-04-23 10:33:09 -0400

    Reorganize file structure; Create basic launch screen

---
+++ Traveling Children Project/TCPTabBarController.swift
@@ -0,0 +1,19 @@
+//  TCPTabBarController.swift
+//  Traveling Children Project
+//
+//  Created by Alexander Rhett Crammer on 4/23/17.
+//  Copyright © 2017 Traveling Children Project. All rights reserved.
+//
+import UIKit
+
+class TCPTabBarController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}

b28b8751548a1c2f88840b6b51444c8eb80d0aa1
Alexander Rhett Crammer <alexander2475914@gmail.com>
2017-04-25 19:12:21 -0400

    Add Twitter-like launch animation

--- Traveling Children Project/AppDelegate.swift
+++ Traveling Children Project/AppDelegate.swift
@@ -7,11 +7,79 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CAAnimationDelegate {
+  // MARK: - Properties
   var window: UIWindow?
 
+  // MARK: - Methods
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window!.backgroundColor = UIColor(red: 157/255, green: 220/255, blue: 249/255, alpha: 1)
+    self.window!.makeKeyAndVisible()
+    
+    // rootViewController from StoryBoard
+    let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    let navigationController = mainStoryboard.instantiateViewController(withIdentifier: "mainTabBarController") as UIViewController
+    self.window!.rootViewController = navigationController
+    
+    // logo mask
+    navigationController.view.layer.mask = CALayer()
+    navigationController.view.layer.mask!.contents = UIImage(named: "Logo")!.cgImage
+    navigationController.view.layer.mask!.bounds = CGRect(x: 0, y: 0, width: 60, height: 60)
+    navigationController.view.layer.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    navigationController.view.layer.mask!.position = CGPoint(x: navigationController.view.frame.width / 2, y: navigationController.view.frame.height / 2)
+    
+    // logo mask background view
+    let maskBgView = UIView(frame: navigationController.view.frame)
+    maskBgView.backgroundColor = UIColor.white
+    navigationController.view.addSubview(maskBgView)
+    navigationController.view.bringSubview(toFront: maskBgView)
+    
+    // logo mask animation
+    let transformAnimation = CAKeyframeAnimation(keyPath: "bounds")
+    transformAnimation.delegate = self
+    transformAnimation.duration = 1
+    transformAnimation.beginTime = CACurrentMediaTime() + 1 //add delay of 1 second
+    let initalBounds = NSValue(cgRect: navigationController.view.layer.mask!.bounds)
+    let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 50, height: 50))
+    let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 2000, height: 2000))
+    transformAnimation.values = [initalBounds, secondBounds, finalBounds]
+    transformAnimation.keyTimes = [0, 0.5, 1]
+    transformAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+    transformAnimation.isRemovedOnCompletion = false
+    transformAnimation.fillMode = kCAFillModeForwards
+    navigationController.view.layer.mask!.add(transformAnimation, forKey: "maskAnimation")
+    
+    // Logo mask background
+    UIView.animate(withDuration: 0.1,
+                    delay: 1.35,
+                    options: UIViewAnimationOptions.curveEaseIn,
+                    animations: {
+                      maskBgView.alpha = 0.0
+                    },
+                    completion: { finished in
+                    maskBgView.removeFromSuperview()
+    })
+    
+    // Root view animation
+    UIView.animate(withDuration: 0.25,
+                    delay: 1.3,
+                    options: [],
+                    animations: {
+                      self.window!.rootViewController!.view.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
+                    },
+                    completion: {
+                      finished in
+
+                      UIView.animate(withDuration: 0.3,
+                                      delay: 0.0,
+                                      options: UIViewAnimationOptions.curveEaseInOut,
+                                      animations: {
+                      }, completion: nil)
+    })
+
     return true
   }
 
@@ -35,5 +103,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+  
+  // MARK: CAAnimationDelegate Methods
+  func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+    // Remove mask when animation completes
+    self.window!.rootViewController!.view.layer.mask = nil
   }
 }

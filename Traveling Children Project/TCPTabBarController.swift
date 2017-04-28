1cd2f260f9ca7d5e5752d91727f11f10659b1774
Alexander Rhett Crammer <Alexander@hotsauceatl.com>
2017-04-28 12:15:45 -0400

    Remove titles from main tab bar and vertically center icons

--- Traveling Children Project/TCPTabBarController.swift
+++ Traveling Children Project/TCPTabBarController.swift
@@ -12,6 +12,14 @@ class TCPTabBarController: UITabBarController {
     // Do any additional setup after loading the view, typically from a nib.
   }
   
+  override func viewDidLayoutSubviews() {
+    // Remove tab bar titles
+    for tab in tabBar.items! {
+      tab.title = nil
+      tab.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+    }
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.

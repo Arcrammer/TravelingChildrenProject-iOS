d432fede08569226b41050b4a70dd1fa42e8db17
Alexander Rhett Crammer <alexander2475914@gmail.com>
2017-04-30 17:27:20 -0400

    Make sign-up form scroll

--- Traveling Children Project/TCPAuthenticationController.swift
+++ Traveling Children Project/TCPAuthenticationController.swift
@@ -17,9 +17,6 @@ class TCPAuthenticationController: UIViewController, UIGestureRecognizerDelegate
     self.view.endEditing(true)
   }
   
-  // MARK: - Properties
-  var keyboardHeight: CGFloat?
-  
   // MARK: - Methods
   override func viewWillAppear(_ animated: Bool) {
     // Give the form view rounded corners
@@ -29,68 +26,37 @@ class TCPAuthenticationController: UIViewController, UIGestureRecognizerDelegate
     NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: .UIKeyboardDidShow, object: nil)
     NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
   }
-  
+
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     // Dismiss the keyboard
     self.view.endEditing(true)
   }
   
   func keyboardDidShow(notification: NSNotification) {
+    // Make sure we have the keyboard frame and the bottom scroll view constraint
+    guard let keyboardFrame = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+          let scrollViewBottom = self.scrollViewBottom else {
+      return
+    }
+    
     // Move the bottom of the scroll view to the top
     // of the keyboard now that we know its' height
-    guard let keyboardFrame = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-      print("Coulnd't get the height of the keyboard")
-      return
-    }
-
-    guard let scrollViewBottom = self.scrollViewBottom else {
-      print("Couldn't get the bottom constraint")
-      return
-    }
-    
-    // Don't change the constraint unless the keyboard frame actually changed
-    let beginFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey]! as AnyObject).cgRectValue!
-    let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey]! as AnyObject).cgRectValue!
-    guard beginFrame.equalTo(endFrame) == false else {
-      print("Keyboard frame didn't change — Skipping scroll bottom view constraint change")
-      return
-    }
-    
-    // Remember how much space we added so we can remove it later — Especially needed in case of rotation changes
-    // since the keyboard height isn't going to match what we had previously added to the bottom constraint
-    self.keyboardHeight = keyboardFrame.size.height
-
-    // Add the keyboards' height to the bottom distance of the scroll view
-    scrollViewBottom.constant += self.keyboardHeight!
+    scrollViewBottom.constant = keyboardFrame.size.height
   }
   
   func keyboardWillHide(notification: NSNotification) {
     // Remove the extra space we added earlier when the keyboard is dismissed
-    guard let keyboardHeight = self.keyboardHeight else {
-      print("Coulnd't get the keyboard height that was supposed to be saved from first showing")
-      return
-    }
-    
     guard let scrollViewBottom = self.scrollViewBottom else {
-      print("Couldn't get the bottom constraint")
       return
     }
     
-    // Don't change the constraint unless the keyboard frame actually changed
-    let beginFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey]! as AnyObject).cgRectValue!
-    let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey]! as AnyObject).cgRectValue!
-    guard beginFrame.equalTo(endFrame) == false else {
-      print("Keyboard frame didn't change — Skipping scroll bottom view constraint change")
-      return
-    }
-
-    // Remove the keyboards' height from the
-    // bottom distance of the scroll view
-    scrollViewBottom.constant -= keyboardHeight
+    // Move the scroll view back to the bottom
+    scrollViewBottom.constant = 0
   }
   
   // MARK: - UITextFieldDelegate Methods
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Either move to the next tagged field or dismiss the keyboard
     if let nextField = textField.superview!.viewWithTag(textField.tag + 1) {
       nextField.becomeFirstResponder()
     } else {

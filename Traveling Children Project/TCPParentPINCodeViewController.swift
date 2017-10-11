08b17873141ed5b26fa8824b91177712bb2f0d50
Alexander Rhett Crammer <Alexander2475914@gmail.com>
2017-10-11 10:26:33 -0400

    ...

--- Traveling Children Project/TCPParentPINCodeViewController.swift
+++ Traveling Children Project/TCPParentPINCodeViewController.swift
@@ -16,27 +16,24 @@ class TCPParentPINCodeViewController: UIViewController, UITextFieldDelegate {
   }
   
   @IBAction func passcodeFieldValueChanged(_ sender: UITextField) {
-    print("passcodeFieldValueChanged()")
     // Either send the user to the next field or verify their passcode
     if sender.text!.count == 1 {
-      print("textField.text!.count == 1")
       if let nextTextField = view.viewWithTag(sender.tag + 1) {
         // Send the user to the next passcode field
         nextTextField.becomeFirstResponder()
-        print("nextTextField.becomeFirstResponder()")
       } else {
         // Dismiss the keyboard because we're on the last passcode bullet
         sender.resignFirstResponder()
-        print("textField.resignFirstResponder()")
       }
     } else if sender.text!.count == 0, let previousTextField = view.viewWithTag(sender.tag - 1) {
       // Send the user to the previous passcode field
       previousTextField.becomeFirstResponder()
-      print("previousTextField.becomeFirstResponder()")
     }
-    print("")
-    print("")
-    print("")
+  }
+
+  @IBAction func submitPasscode() {
+    print("We're going to pretend the passcode was right")
+    performSegue(withIdentifier: "TCPEditReadingLogSegue", sender: self)
   }
   
   // MARK: - Methods
@@ -47,16 +44,14 @@ class TCPParentPINCodeViewController: UIViewController, UITextFieldDelegate {
   
   // MARK: - UITextFieldDelegate Methods
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-    print("shouldChangeCharactersIn range")
     // If the user hit the back button, the current passcode field is
     // empty, and there's a previous passcode field send the user to it
     if strcmp(string.cString(using: String.Encoding.utf8)!, "\\b") == -92,
       textField.text! == "",
       let previousTextField = view.viewWithTag(textField.tag - 1) {
       previousTextField.becomeFirstResponder()
-      print("previousTextField.becomeFirstResponder()")
     }
-    
+
     return true
   }
 }
